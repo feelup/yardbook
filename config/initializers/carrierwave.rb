e7e5839819a3f88680152df30fc688281761df21
feelup <phillip.k.han@gmail.com>
2019-05-03 19:01:47 +0900

    corrected single quote issue in carrierwave initializer

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,14 +4,14 @@ require 'carrierwave/storage/fog'
 
 CarrierWave.configure do |config|
   if Rails.env.staging? || Rails.env.production?
-    config.fog_provider = ‘fog/aws’ 
+    config.fog_provider = 'fog/aws' 
     config.fog_credentials = {
-      :provider => ‘AWS’,
-      :aws_access_key_id => ENV[‘AWS_ACCESS_KEY_ID’],
-      :aws_secret_access_key => ENV[‘AWS_SECRET_ACCESS_KEY’],
-      :region => ‘ap-northeast-2’
+      :provider => 'AWS',
+      :aws_access_key_id => ENV['AWS_ACCESS_KEY_ID'],
+      :aws_secret_access_key => ENV['AWS_SECRET_ACCESS_KEY'],
+      :region => 'ap-northeast-2'
     }
-    config.fog_directory = ENV[‘S3_BUCKET_NAME’]
+    config.fog_directory = ENV['S3_BUCKET_NAME']
     config.storage = :fog
   else
     config.storage = :file
